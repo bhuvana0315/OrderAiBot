7f9b1d6c1819ceb0da9b055a64a137cb9bd6aab9
Gayathri0315 <125669820+Gayathri0315@users.noreply.github.com>
2023-06-19 17:44:16 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = ""
 
 def get_completion_from_message(message, model="davinci", temperature=0.5):
     response = openai.Completion.create(
