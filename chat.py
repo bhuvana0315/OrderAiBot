201f0540688bb282a992c33229bbf591c0efea5d
BhuvanaGayathri Kanakala <bhuvana0315@gmail.com>
2023-07-25 16:28:26 +0000

    added_order_summary

--- chat.py
+++ chat.py
@@ -1,6 +1,8 @@
 import openai
 import os
 from dotenv import load_dotenv
+import json
+from twilio.rest import Client
 
 load_dotenv()
 
@@ -33,7 +35,6 @@ Ravva dosa   20.00 \
 Onion Ravva dosa 30.00 \
 Egg dosa 45.00
 pesarattu    35.00 \
-
 
 Drinks: \
 bottled water 30.00 \
@@ -68,7 +69,155 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
 
 def collect_messages_text(msg):
     prompt = msg
+    if(prompt=="pickup" or prompt=="delivery"):
+        store_order_summary()
     context.append({'role':'user', 'content':f"{prompt}"})
     response = get_completion_from_messages(context) 
     context.append({'role':'assistant', 'content':f"{response}"})
     return response
+
+
+def store_order_summary():
+    context.append({'role':'user','content':'Store the order in a json format with fields containing items,quantity and total price'})
+    response = get_completion_from_messages(context) 
+    # context.append({'role':'assistant', 'content':f"{response}"})
+    print(response)
+    with open('order_summary.json', 'w') as json_file:
+        json.dump(response, json_file)
+    user_phone_number='+917386785251'
+    send_whatsapp_message(user_phone_number, response)
+
+def send_whatsapp_message(to, body):
+    # Twilio credentials
+    twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    twilio_whatsapp_number = os.getenv("TWILIO_WHATSAPP_NUMBER")
+    # twilio_whatsapp_number = 'whatsapp:+14155238886'  # Twilio's sandbox WhatsApp number
+
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    try:
+        message = client.messages.create(
+            from_=twilio_whatsapp_number,
+            to='whatsapp:' + to,
+            body=body
+        )
+
+        print('WhatsApp message sent successfully.')
+        print(message.sid)
+    except Exception as e:
+        print('Error sending WhatsApp message:', str(e))
+
+
+
+
+
+
+# import os
+# from dotenv import load_dotenv
+# from twilio.rest import Client
+
+# load_dotenv()
+
+# # Rest of your code using the OpenAI library
+
+# def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages,
+#         temperature=temperature,
+#     )
+#     return response.choices[0].message["content"]
+
+
+# def send_whatsapp_message(to, body):
+#     account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+#     auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+#     twilio_whatsapp_number = os.getenv("TWILIO_WHATSAPP_NUMBER")
+
+#     client = Client(account_sid, auth_token)
+
+#     try:
+#         message = client.messages.create(
+#             from_=twilio_whatsapp_number,
+#             to='whatsapp:' + to,
+#             body=body
+#         )
+
+#         print('WhatsApp message sent successfully.')
+#         print(message.sid)
+#     except Exception as e:
+#         print('Error sending WhatsApp message:', str(e))
+
+
+# # d
+
+# context = [{'role': 'system', 'content': """
+#         You are OrderBot, an automated service to collect orders for a street dosa. \
+#         Your first question after greeting the customer is, "How may I help you today?" \
+#         You collect the order, and then ask if it's a pickup or delivery. \
+#         You wait to collect the entire order, then summarize it and check for the final total. \
+#         You ask if the customer wants to add anything else. \
+#         Make sure to clarify all options, extras, and sizes and uniquely identify the item from the menu. \
+#         If it's a delivery, you ask for an address. \
+#         Finally, you collect the payment for all the orders. \
+#         You should respond only to take orders, and for all other questions, you should not respond since you are an orderbot. \
+#         You respond in a short, very conversational, friendly style. \
+#         You should take orders only for the items that are included in the following menu. \
+#         The menu includes: \
+
+#         Masala dosa  40.00 \
+#         Onion dosa   25.00 \
+#         Plain dosa   20.00 \
+#         Ravva dosa   20.00 \
+#         Onion Ravva dosa 30.00 \
+#         Egg dosa 45.00 \
+#         Pesarattu    35.00 \
+
+#         Drinks: \
+#         Bottled water 30.00 \
+#         Tea: \
+#         Normal Tea : 10.00 \
+#         Special Tea: 20.00 \
+#         Elachi Tea: 15.00 \
+#         Green Tea : 15.00 \
+#         Coffee: \
+#         Normal Coffee: 15.00 \
+#         Filtered Coffee: 30.00 \
+#         Black Coffee : 20.00 \
+#         Cool drinks: \
+#         Sprite 10.00, 30.00, 50.00 \
+#         Thums Up 10.00, 30.00, 50.00 \
+#         Pepsi 10.00, 30.00, 50.00 \
+#         Maaza 10.00, 30.00, 50.00 \
+#         Slice 10.00, 30.00, 50.00 \
+#         """}]
+
+# def collect_messages_text(msg, user_phone_number=None):
+#     # Check if the user provided their phone number in the message
+#     if user_phone_number is None:
+#         user_phone_number = extract_phone_number_from_message(msg)
+
+#     prompt = msg
+#     context.append({'role': 'user', 'content': f"{prompt}"})
+#     response = get_completion_from_messages(context)
+#     context.append({'role': 'assistant', 'content': f"{response}"})
+
+#     # Send the response as a WhatsApp message
+#     if user_phone_number:
+#         send_whatsapp_message(user_phone_number, response)
+
+#     return response
+
+# import re
+
+# def extract_phone_number_from_message(msg):
+#     # Use regular expression to find a phone number pattern
+#     phone_number_pattern = r'\+?\d{1,3}[-.\s]?\(?\d{1,5}\)?[-.\s]?\d{1,5}[-.\s]?\d{1,5}[-.\s]?\d{1,9}'
+#     matches = re.findall(phone_number_pattern, msg)
+
+#     # Return the first phone number found, if any
+#     if matches:
+#         return matches[0]
+
+#     return None
--- main.py
+++ main.py
@@ -18,9 +18,12 @@ def home(request: Request):
 @app.post("/chat")
 async def chat(message: Message):
     user_message = message.content
+    # user_phone_number = "RECEIVER_PHONE_NUMBER_WITH_COUNTRY_CODE"
+    
     response = collect_messages_text(user_message)
 
     return {"message": response}
+
 
 @app.post("/process_voice")
 async def process_voice(voice_input: dict):
@@ -32,6 +35,53 @@ async def process_voice(voice_input: dict):
     # print(response)
     return {"message": response}
 
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, port=8000)
+
+
+# from fastapi import FastAPI, Request
+# from fastapi.templating import Jinja2Templates
+# from pydantic import BaseModel
+# from fastapi.staticfiles import StaticFiles
+# from chat import get_completion_from_messages, collect_messages_text
+
+# app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
+
+
+# class Message(BaseModel):
+#     content: str
+#     user_phone_number: str
+
+
+# @app.get("/")
+# def home(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# @app.post("/chat")
+# async def chat(message: Message):
+#     user_message = message.content
+#     user_phone_number = message.user_phone_number
+#     response = collect_messages_text(user_message, user_phone_number)
+
+#     return {"message": response}
+
+
+# @app.post("/process_voice")
+# async def process_voice(voice_input: dict):
+#     # Process the voice input as needed
+#     text = voice_input.get('input')
+#     # Call the function to handle the conversation
+#     user_phone_number = voice_input.get('user_phone_number')
+#     response = collect_messages_text(text, user_phone_number)
+#     return {"message": response}
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, port=8000)
